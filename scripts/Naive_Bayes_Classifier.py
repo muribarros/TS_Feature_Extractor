084d6eee609c9662216e91ba04b773655fe4f9ae
Murilo costa de barros <83475598+muribarros@users.noreply.github.com>
2024-12-19 13:37:24 +0800

    Update Naive_Bayes_Classifier.py

--- scripts/Naive_Bayes_Classifier.py
+++ scripts/Naive_Bayes_Classifier.py
@@ -3,10 +3,9 @@
 #-------------------------------------//--------------------------------------
 
 #----------------------
-# BIBLIOTECA
+# Import
 #----------------------
 
-# Imports
 #from sklearn.datasets import make_blobs
 #from sklearn.model_selection import train_test_split
 import numpy as np
@@ -25,7 +24,7 @@ comZscore = True
 scaler = StandardScaler()
 
 
-regioes = np.load('Lista_final_interseccao.npy')    
+regions = np.load('Lista_final_interseccao.npy')    
 ids_df = pd.read_csv('id_region.csv', sep = ';')
 
 gt_label = np.ones(68)
@@ -33,15 +32,15 @@ gt_label[0:34] = gt_label[0:34] * 0
 
 lista = []
 
-for regiao in regioes:
-    matriz = np.load('/Users/Lab/Documents/Murilo/'+ tech +'_'+ mod + '_Matriz/Matriz_' + str(regiao) + '.npy')
+for region in regions:
+    matriz = np.load('/Users/Lab/Documents/Murilo/'+ tech +'_'+ mod + '_Matriz/Matriz_' + str(region) + '.npy')
      # Initialize SVM classifier
     if comZscore:
         scaler.fit(matriz)
         matriz = scaler.transform(matriz)
         
     clf = GaussianNB()
-    #print(regiao)
+    #print(region)
 # Fit data
     clf = clf.fit(matriz, gt_label)   
 # Predict the test set
@@ -55,15 +54,15 @@ for regiao in regioes:
     #print('Recall: %f' % recall)
     f1= 2*((precision*recall)/(precision+recall))
     #print('F1 score: %f' % f1)
-    #print('%d,%f,%f,%f,%f' % (regiao, accuracy, precision, recall, f1)) 
+    #print('%d,%f,%f,%f,%f' % (region, accuracy, precision, recall, f1)) 
 
-    lista.append([int(regiao), accuracy, precision, recall, f1])
+    lista.append([int(region), accuracy, precision, recall, f1])
 
-results = np.asarray(lista)
+results = np.asarray(list)
 
 results_df = pd.DataFrame(results)
 results_df.columns = ['Region', 'accuracy', 'precision', 'recall', 'f1']
 ids_df = ids_df.merge(results_df, how='left', on='Region')
 ids_df = ids_df.sort_values(by='Region')
 
-ids_df.to_csv('NB_'+tech + '_' + mod + '_' + str(comZscore)+ '.csv')
+ids_df.to_csv('NB_'+tech + '_' + mod + '_' + str(withZscore)+ '.csv')
